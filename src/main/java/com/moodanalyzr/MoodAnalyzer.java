9c762be6274824e60d519dc30ae70c5080c6ba68
MKS1998 <nksayyad2900@outlook.com>
2021-10-14 13:23:55 +0530

    Added : Program For The Create The Object For The Mood Analyzer

--- src/main/java/com/moodanalyzr/MoodAnalyzer.java
+++ src/main/java/com/moodanalyzr/MoodAnalyzer.java
@@ -1,28 +1,13 @@
 package com.moodanalyzr;
 
-
 public class MoodAnalyzer {
-    private String message;
+	public MoodAnalyzer() {
+	}
 
-    public MoodAnalyzer() {
-    }
-
-    public MoodAnalyzer(String message) {
-        this.message = message;
-    }
-
-    public String analyseMood() throws MoodAnalysisException {
-        try {
-
-            if (this.message.contains("Sad"))
-                return "SAD";
-            else
-                return "HAPPY";
-        } catch (Exception e) {
-            if (message == null)
-                throw new MoodAnalysisException("Invalid Mood", MoodAnalysisException.Exception_Type.NULL);
-            else
-                throw new MoodAnalysisException("Invalid Mood", MoodAnalysisException.Exception_Type.EMPTY);
-        }
-    }
+	public String moodAnalyzer(String message) {
+		if (message.contains("Sad"))
+			return "SAD";
+		else
+			return "HAPPY";
+	}
 }
